4ec5ef88d126162ac44284992fd973c7f7b8fe50
t4kaha00 <hkarmacharya@gmail.com>
2019-04-13 01:47:29 +0300

    Updates

---
+++ precache-manifest.dc3908ae6ec0d3e217bc95ecaeeeab82.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "db8c9e22b7cb7ebe25ae3aef49efa5ac",
+    "url": "/portfolio/static/media/4.db8c9e22.jpg"
+  },
+  {
+    "revision": "fbc2f699cf81371af60f",
+    "url": "/portfolio/static/css/main.99514025.chunk.css"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/portfolio/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "326d9a7f41be2b869f3949271148660a",
+    "url": "/portfolio/static/media/7.326d9a7f.jpg"
+  },
+  {
+    "revision": "7cadac15d8ba8e5bfda98ff031e9ffb7",
+    "url": "/portfolio/static/media/5.7cadac15.jpg"
+  },
+  {
+    "revision": "fbc2f699cf81371af60f",
+    "url": "/portfolio/static/js/main.c533508b.chunk.js"
+  },
+  {
+    "revision": "d8f65ec87f5924dc3616bf3a6199932f",
+    "url": "/portfolio/static/media/3.d8f65ec8.jpg"
+  },
+  {
+    "revision": "88018307aaf9491a5c4ea3fba58cd756",
+    "url": "/portfolio/static/media/1.88018307.jpg"
+  },
+  {
+    "revision": "5c09bc134bf6a943ff5b5bbfdb896d0a",
+    "url": "/portfolio/static/media/2.5c09bc13.jpg"
+  },
+  {
+    "revision": "084cc197a73bf380287ad0745acc9dd6",
+    "url": "/portfolio/static/media/6.084cc197.jpg"
+  },
+  {
+    "revision": "64fdc7e9c680c602667510855703b044",
+    "url": "/portfolio/index.html"
+  }
+];
