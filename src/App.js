fce86d76914df5dfe30b3ebedec75c39afa74e98
t4kaha00 <hkarmacharya@gmail.com>
2019-04-22 12:40:03 +0300

    Popup for contact added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = { open : true };
+  }
+
+  hide() {
+    // Changes the state to opposite of current state
+    this.setState({
+      open: !this.state.open
+    });
+    
+    setTimeout(function(){
+      console.log("clicked");
+    }, 1000);
+    
+  }
+
   render() {
+    
+    var open = {
+      // Changes the visibility from visible to collapsed. Collapsed is used to maintain the content area
+      visibility: this.state.open ? "visible" : "collapse"
+    };
+
     return (
       <div className="App">
+      {/* Sidebar */}
         <div className="sidebar">
+        {/* Logo */}
           <div className="logo">
             <a><img src="h.jpg" alt="logo"/></a>
           </div>
+          {/* Navigation */}
           <nav>
             <a href="#" className="nav-item">About</a>
             <a href="#" className="nav-item">Portfolio</a>
@@ -17,9 +45,10 @@ class App extends Component {
           </nav>
         </div>
 
+        {/* Content Area */}
         <div className="content">
           <div className="portfolio">
-            <div className="portfolio-item one"></div>
+            <div className="portfolio-item one" style={open} onClick={this.hide.bind(this)}></div>
             <div className="portfolio-item two"></div>
             <div className="portfolio-item three"></div>
             <div className="portfolio-item four"></div>
