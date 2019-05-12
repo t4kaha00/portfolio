c1f724dff790d5c5471a660178c28c58edece997
t4kaha00 <hkarmacharya@gmail.com>
2019-05-12 21:11:25 +0300

    Routing pages initialize

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import ReactDOM from 'react-dom';
+import { Document, Page } from 'react-pdf';
+import resume from './resume.pdf'
 import './App.css';
 
 class App extends Component {
@@ -9,60 +12,65 @@ class App extends Component {
     this.state = { open : true };
   }
 
-  hide() {
-    // Changes the state to opposite of current state
-    this.setState({
-      open: !this.state.open
-    });
-    
-    setTimeout(function(){
-      console.log("clicked");
-    }, 1000);
-    
-  }
-
   render() {
-    
-    var open = {
-      // Changes the visibility from visible to collapsed. Collapsed is used to maintain the content area
-      visibility: this.state.open ? "visible" : "collapse"
-    };
-
     return (
       <div className="App">
-      {/* Sidebar */}
+      <Router>
+        {/* Sidebar */}
         <div className="sidebar">
-        {/* Logo */}
+        
+          {/* Logo */}
           <div className="logo">
             <a><img src="h.jpg" alt="logo"/></a>
           </div>
+          
           {/* Navigation */}
-          <nav>
-            <a href="#" className="nav-item">About</a>
-            <a href="#" className="nav-item">Portfolio</a>
-            <a href="#" className="nav-item contact">Contact</a>
-            <a href="https://github.com/t4kaha00" className="nav-item">Github</a>
-          </nav>
+            <nav>  
+                <Link to="/" className="nav-item">About</Link>
+                <Link to="/portfolio" className="nav-item">Portfolio</Link>
+                <a href="mailto:hkarmacharya@gmail.com" className="nav-item contact">Contact</a>
+                <a href="https://github.com/t4kaha00" className="nav-item">Github</a>
+            </nav>
         </div>
 
-        {/* Content Area */}
-        <div className="content">
-          <div className="portfolio">
-            <div className="portfolio-item one" style={open} onClick={this.hide.bind(this)}></div>
-            <div className="portfolio-item two"></div>
-            <div className="portfolio-item three"></div>
-            <div className="portfolio-item four"></div>
-            <div className="portfolio-item five"></div>
-            <div className="portfolio-item six"></div>
-            <div className="portfolio-item seven"></div>
-            <div className="portfolio-item eight"></div>
-            <div className="portfolio-item nine"></div>    
-          </div>
-        </div>
-        
+        <Route exact path="/about" component={About} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
+        </Router>
+
+       
+
       </div>
     );
   }
+}
+
+function Portfolio() {
+ 
+  return (
+    //  Content area
+     <div className="content">
+     <div className="portfolio">
+       <div className="portfolio-item one" ></div>
+       <div className="portfolio-item two"></div>
+       <div className="portfolio-item three"></div>
+       <div className="portfolio-item four"></div>
+       <div className="portfolio-item five"></div>
+       <div className="portfolio-item six"></div>
+       <div className="portfolio-item seven"></div>
+       <div className="portfolio-item eight"></div>
+       <div className="portfolio-item nine"></div>    
+     </div>
+   </div>
+  );
+}
+
+function About() {
+
+}
+
+function Contact() {
+
 }
 
 export default App;
