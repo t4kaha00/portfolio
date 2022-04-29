c2ec7c652153914fbc3c670a480b409b75ff3d73
t4kaha00 <hkarmacharya@gmail.com>
2022-04-29 12:48:33 +0300

    Hover effects, logo and redirection added

--- src/App.js
+++ src/App.js
@@ -41,7 +41,9 @@ class App extends Component {
     return (
       <div className="App">
         <div className='resume'>
+          {/* Left column */}
           <div className='left'>
+            {/* Details */}
             <div className='col1-row1 details'>
               <div className='col1-row1-row'>
                 <h1>Harjit Karmacharya</h1>
@@ -64,6 +66,8 @@ class App extends Component {
                 </div>
               </div>
             </div>
+
+            {/* Skills */}
             <div className='col1-row2 skills'>
               <div className='col1-row2-row'>
                <h2><u>SKILLS</u></h2>
@@ -99,17 +103,31 @@ class App extends Component {
                 </ul>
               </div>
             </div>
+            {/* Languages */}
             <div className='col1-row3 languages'>
               <div className='col1-row3-row'>
                 <h2><u>Languages</u></h2> 
               </div>
               <div className='col1-row3-row'>
-                <p>English</p>
-                <p>Finnish</p>
-                <p>Nepali</p>
-                <p>Hindi</p>
+                <div>
+                  <p>English</p>
+                  <p className='dots'>••••◦</p>
+                </div>
+                <div>
+                  <p>Finnish</p>
+                  <p className='dots'>••◦◦◦</p>
+                </div>
+                <div>
+                  <p>Nepali</p>
+                  <p className='dots'>•••••</p>
+                </div>
+                <div>
+                  <p>Hindi</p>
+                  <p className='dots'>••••◦</p>
+                </div>
               </div>
             </div>
+            {/* Live Projects */}
             <div className='col1-row4 live-projects'>
               <div className='col1-row4-row'>
                 <h2><u>LIVE PROJECTS</u></h2>
@@ -117,93 +135,120 @@ class App extends Component {
               <div className='col1-row4-row'>
                 <ul>
                   <li>
-                    <a href='https://nadaasi.com'>nadaasi.com</a>
+                    <a href='https://nadaasi.com'><img src='nadaasi.png' width={100}/></a>
                   </li>
                   <br/>
                   <li>
-                    <a href='https://lianatech.herokuapp.com'>lianatech.herokuapp.com</a>
+                    <a href='https://lianatech.herokuapp.com'><img src='liana.svg' width={100}/></a>
                   </li>
                 </ul>
               </div>
             </div>
           </div>
 
-
+          {/* Right Column */}
           <div className='right'>
             <div className='col2-row1'>
+              {/* Education */}
               <h2 className='col2-row1-row right-heading2'><u>EDUCATION HISTORY</u></h2>
               <div className='col2-row1-row ouas'>
-                <div className='col2-row1-row-row'>
-                  <h4 className='heading1'>Bachelors of Engineering in Information and Communications Technology</h4>
-                  <p>Aug 2014 - Aug 2020</p>
-                </div>
-                <div className='col2-row1-row-row'>
-                  <h3>Oulu University of Applied Sciences</h3>
+                <div>
+                  <a href='https://www.oamk.fi/'>
+                    <div className='col2-row1-row-row'>
+                      <h4 className='heading1'>Bachelors of Engineering in Information and Communications Technology</h4>
+                      <p>Aug 2014 - Aug 2020</p>
+                    </div>
+                    <div className='col2-row1-row-row'>
+                      <h3>Oulu University of Applied Sciences</h3>
+                    </div>
+                  </a>
                 </div>
               </div>
               <div className='col2-row1-row dit'>
-                <div className='col2-row1-row-row'>
-                  <h4 className='heading1'>Bachelors in Computer Science</h4>
-                  <p>Sep 2016 - May 2017</p>
-                </div>
-                <div className='col2-row1-row-row'>
-                  <h3>Dublin Institute of Technology</h3>
-                  <i>Erasmus Computing</i>
+                <div>
+                  <a href='https://www.tudublin.ie/'>
+                    <div className='col2-row1-row-row'>
+                      <h4 className='heading1'>Bachelors in Computer Science</h4>
+                      <p>Sep 2016 - May 2017</p>
+                    </div>
+                    <div className='col2-row1-row-row'>
+                      <h3>Dublin Institute of Technology</h3>
+                      <i>Erasmus Computing</i>
+                    </div>
+                  </a>
                 </div>
               </div>
             </div>
             <div className='col2-row2'>
-              <h2 className='col2-row2-row right-heading2'><u>WORK EXPERIENCE</u></h2>
+              {/* Work */}
+              <h2 className='right-heading2'><u>WORK EXPERIENCE</u></h2>
               <div className='col2-row2-row cajo'>
-                <div className='col2-row2-row-row'>
-                  <h4 className='right-heading1'>IMAGE MODIFICATION PORTAL</h4>
-                  <p>Jan 2017 - April 2017</p>
-                </div>
-                <div className='col2-row2-row-row'>
-                  <h2>Cajo Oy</h2>
-                  <ul>
-                    <li><p>Mapping images into 3D objects online</p></li>
-                  </ul>
+                <div>
+                  <a href='https://cajotechnologies.com/'>
+                    <div className='col2-row2-row-row'>
+                      <h4 className='right-heading1'>IMAGE MODIFICATION PORTAL</h4>
+                      <p>Jan 2017 - April 2017</p>
+                    </div>
+                    <div className='col2-row2-row-row'>
+                      <h2>Cajo Oy</h2>
+                      <ul>
+                        <li><p>Mapping images into 3D objects online</p></li>
+                      </ul>
+                    </div>
+                  </a>
                 </div>
               </div>
               <div className='col2-row2-row nepgo'>
-                <div className='col2-row2-row-row'>
-                  <h4 className='right-heading1'>ANDROID APPLICATION</h4>
-                  <p>Jan 2017 - Mar 2017</p>
-                </div>
-                <div className='col2-row2-row-row'>
-                  <h2>Nepgo Oy</h2>   
-                  <ul>
-                    <li><p>A demo android application for the startup company oriented for charities</p></li>
-                  </ul>
+                <div>
+                  <a href='https://www.nepgo.com/'>
+                    <div className='col2-row2-row-row'>
+                      <h4 className='right-heading1'>ANDROID APPLICATION</h4>
+                      <p>Jan 2017 - Mar 2017</p>
+                    </div>
+                    <div className='col2-row2-row-row'>
+                      <h2>Nepgo Oy</h2>   
+                      <ul>
+                        <li><p>A demo android application for the startup company oriented for charities</p></li>
+                      </ul>
+                    </div>
+                  </a>
                 </div>
               </div>
               <div className='col2-row2-row nadaasi'>
-                <div className='col2-row2-row-row'>
-                  <h4 className='right-heading1'>WEBSHOP</h4>
-                  <p>Jun 2020 - Dec 2020</p>
-                </div>
-                <div className='col2-row2-row-row'>
-                  <h2>Nadaasi Oy</h2>
-                  <ul>
-                    <li><p>Modern webshop based on javascript and mngoDB</p></li>
-                  </ul>
+                <div>
+                  <a href='https://nadaasi.com/'>
+                    <div className='col2-row2-row-row'>
+                      <h4 className='right-heading1'>WEBSHOP</h4>
+                      <p>Jun 2020 - Dec 2020</p>
+                    </div>
+                    <div className='col2-row2-row-row'>
+                      <h2>Nadaasi Oy</h2>
+                      <ul>
+                        <li><p>Modern webshop based on javascript and mngoDB</p></li>
+                      </ul>
+                    </div>
+                  </a>
                 </div>
               </div>
               <div className='col2-row2-row nclean'>
-                <div className='col2-row2-row-row'>
-                  <h4 className='right-heading1'>SUPERVISOR</h4>
-                  <p>Jan 2019 - Apr 2021</p>
-                </div>
-                <div className='col2-row2-row-row'>
-                  <h2>Nclean Oy</h2>
-                  <ul>
-                    <li><p>Managing the housekeeping department for client hotels (Aalto Inn, Unihome Oy)</p></li>
-                  </ul>
+                <div>
+                  <a href='https://nadaasi.com/'>
+                    <div className='col2-row2-row-row'>
+                      <h4 className='right-heading1'>SUPERVISOR</h4>
+                      <p>Jan 2019 - Apr 2021</p>
+                    </div>
+                    <div className='col2-row2-row-row'>
+                      <h2>Nclean Oy</h2>
+                      <ul>
+                        <li><p>Managing the housekeeping department for client hotels (Aalto Inn, Unihome Oy)</p></li>
+                      </ul>
+                    </div>
+                  </a>
                 </div>
               </div>
             </div>
             <div className='col2-row3'>
+              {/* Projects */}
               <h2 className='col2-row3-row right-heading2'><u>PROJECTS</u></h2>
               <div className='col2-row3-row'>
                 <div className='col2-row3-row-row'>
