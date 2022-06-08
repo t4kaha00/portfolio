abdf3814af034cd6e8fa1ed06607de9414ed27b8
Harjit Karmacharya <hkarmacharya@gmail.com>
2022-06-09 01:41:09 +0300

    server js edited to change the server port and add a production build file

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,18 @@ const dotenv = require('dotenv')
 const routesUrls = require('./routes/routes')
 const cors = require('cors')
 
+const PORT = process.env.PORT || 8080;
+
 dotenv.config()
 
 mongoose.connect(process.env.DATABASE_ACCESS, () => console.log("Database Connected"))
 
 app.use(express.json())
 app.use(cors())
+
+// In production mode, use build folder for staic web for faster loading
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../build'));
+}
 app.use('/app', routesUrls)
-app.listen(4000, () => console.log("server is up and running"))
+app.listen(PORT, () => console.log(`Server is starting at ${PORT}`))
