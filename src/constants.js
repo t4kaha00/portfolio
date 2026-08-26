f0b69499008774b2e1270cbad42777ea113b2de0
Harjit <hkarmacharya@gmail.com>
2026-08-26 12:40:59 +0300

    Separate GitHub URL into personal and professional
    Split GITHUB_URL into personal and professional URLs.

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 export const EMAIL = 'hkarmacharya@gmail.com'
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/harjit-karmacharya'
-export const GITHUB_URL = 'https://github.com/t4kaha00'
+export const GITHUB_URL_PERSONAL = 'https://github.com/t4kaha00'
+export const GITHUB_URL_PROFESSIONAL = 'https://github.com/t4kaha00'
 export const RESUME_PDF_URL = 'harjitkarmacharya.pdf'
 
 // Base URL of the site hosting the Netlify functions.
